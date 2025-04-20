d80d559051334394b0a342d20c34f44df346530c
shailesh-04 <shailesakoli@gmail.com>
2025-04-20 15:40:22 +0530

    Create React App Using TypeScript

--- SERVER/src/scripts/migrations.ts
+++ SERVER/src/scripts/migrations.ts
@@ -1,17 +1,6 @@
 import computers,{ computersMigration } from "@migrations/computers";
 
 async function callApi() {
-    try {
-        
-        await computers.create({
-            brand:"",
-            model:"",
-            price:0
-        });
-        console.log("Successfuly Insert New Record");
-    } catch (error) {
-        
-    }
     const result = await computersMigration.sql("select * from computers");
     console.log(result);   
     process.exit();
